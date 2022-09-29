206f240bcfad7539a2a8fe4773707b8f5a1ca0c7
yardenedry <yardenedry0@gmail.com>
2022-09-28 21:39:42 -0400

    webpack config

---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: process.env.NODE_ENV,
+  entry: path.resolve(__dirname, "./src/index.js"),
+  output: {
+    path: path.resolve(__dirname, "./dist"),
+    filename: "bundle.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env", "@babel/preset-react"],
+          },
+        },
+      },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader", "postcss-loader"],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ["*", ".js", ".jsx"],
+  },
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, "./dist"),
+    },
+    historyApiFallback: true,
+  },
+  // plugins: [
+  //   new HtmlWebpackPlugin({
+  //     template: path.resolve(__dirname, './dist/index.html')
+  //   })
+  // ],
+};
