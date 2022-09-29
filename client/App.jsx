88495bdc0e225d4bf07e85f627f0ca3b86604ca7
Judy <judywuxingyi@gmail.com>
2022-09-28 22:24:04 -0400

    set up react front-end and express server connection
    Co-authored-by: yardenedry <yardenedry0@gmail.com>
    Co-authored-by: victorhe33 <victorhe33@gmail.com>
    Co-authored-by: hwhalse <halsestrengthfitness@gmail.com>
    Co-authored-by: judywuxingyi <41937921+judywuxingyi@users.noreply.github.com>
    Co-authored-by: MarikoIwata <3701668+MarikoIwata@users.noreply.github.com>

---
+++ client/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+
+// import components here
+// component A, B, C...
+
+function App () {
+    return (
+        <div>
+            <div>hello world</div>
+            <Routes>
+                {/* <Route>add route here</Route> */}
+            </Routes>
+        </div>
+
+    );
+}
+
+export default App;
