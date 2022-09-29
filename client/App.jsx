6c60fdbb0f7501277eb4c5f53f2d553f888d9847
Judy <judywuxingyi@gmail.com>
2022-09-28 22:55:16 -0400

    set up react front-end and express server connection
    Co-authored-by: yardenedry <yardenedry0@gmail.com>
    Co-authored-by: victorhe33 <victorhe33@gmail.com>
    Co-authored-by: hwhalse <halsestrengthfitness@gmail.com>
    Co-authored-by: judywuxingyi <41937921+judywuxingyi@users.noreply.github.com>
    Co-authored-by: MarikoIwata <3701668+MarikoIwata@users.noreply.github.com>

--- client/App.jsx
+++ client/App.jsx
@@ -1,14 +1,16 @@
-import React from 'react';
+import React, { useNavigate } from 'react';
 import { Routes, Route } from 'react-router-dom';
 
 // import components here
 // component A, B, C...
 
 function App () {
+    // <Link path="/users">click here for users</Link>
     return (
         <div>
             <div>hello world</div>
             <Routes>
+                <Route path="/users"><UserComponent></UserComponent></Route>
                 {/* <Route>add route here</Route> */}
             </Routes>
         </div>
