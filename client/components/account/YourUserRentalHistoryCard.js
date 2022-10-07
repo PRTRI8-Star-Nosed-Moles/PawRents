3ec96494b629614885b34ad92b6ee76392c28397
Mariko Iwata <3701668+MarikoIwata@users.noreply.github.com>
2022-10-07 09:42:16 -0400

    updated date format in YourUserRentalHistoryCard

--- client/components/account/YourUserRentalHistoryCard.js
+++ client/components/account/YourUserRentalHistoryCard.js
@@ -2,21 +2,22 @@ import React from 'react';
 
 export const YourUserRentalHistoryCard = (props) => {
     const {
-        _id,
         date,
-        pet_id,
-        username
+        pet_id
     } = props.obj
 
     //need to link pet_id with pet name to render name here
+    
+    const resDate = new Date(date)
+    const day = resDate.getDate()
+    const month = resDate.getMonth()
+    const year = resDate.getFullYear()
 
     return (
         <div className="account-pet-card" >
             <ul>
-            <li>_id: {_id}</li>
-                
-                <li>date: {date}</li>
-                <li>pet id: {pet_id}</li>
+                <li>Date: {month + '/'+day + '/' + year}</li>
+                <li>You are renting: {pet_id}</li>
             </ul>         
         </div>
     )
