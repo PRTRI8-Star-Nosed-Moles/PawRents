0ef20bc3592b02e70ce3a4b73adf33947066001e
hwhalse <halsestrengthfitness@gmail.com>
2022-10-06 20:24:17 -0400

    updated accounts

--- client/components/account/Account.js
+++ client/components/account/Account.js
@@ -1,13 +1,32 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { AddPet } from "./AddPet";
 import { RentalHistory } from "./RentalHistory";
 import { useLocation } from "react-router-dom";
+import { MyPets } from "./MyPets";
 
 export const Account = () => {
     const location = useLocation();
     // const username = sessionStorage.getItem('username');
+    const [myPets, setMyPets] = useState([])
 
     console.log(sessionStorage.getItem('username'))
+    const username = sessionStorage.getItem('username')
+
+    const fetchPets = async () => {
+      try {
+        const data = await fetch(`/api/pet/mypets/${username}`, {
+          method: "GET"
+        });
+        const response = await data.json();
+        setMyPets(response)
+      } catch(err) {
+        console.log(err)
+      }
+    }
+
+    useEffect(() => {
+      fetchPets()
+    }, [])
 
     const [petAdd, setPetAdd] = useState(false)
 
@@ -17,6 +36,7 @@ export const Account = () => {
     return (
         <div>
           <h1>Account</h1>
+          {myPets.map((pet, i) => <MyPets key={i} pet={pet}/>)}
           {petAdd === false ? '' : <AddPet/>}
           <button onClick={changeAdd}>Add Pet</button>
           <div id="account-history">
